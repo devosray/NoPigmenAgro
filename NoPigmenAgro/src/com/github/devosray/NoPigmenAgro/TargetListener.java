b896b7864fbb2f55c1fc7227766e7479dfb4d5dd
Raymond de Vos <devosray@gmail.com>
2013-10-01 17:49:47 +0200

    Disabled damage

--- NoPigmenAgro/src/com/github/devosray/NoPigmenAgro/TargetListener.java
+++ NoPigmenAgro/src/com/github/devosray/NoPigmenAgro/TargetListener.java
@@ -3,9 +3,11 @@ package com.github.devosray.NoPigmenAgro;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.PigZombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
 
 /**
@@ -26,9 +28,23 @@ public class TargetListener implements Listener{
        if (evt.getEntity().getType() == EntityType.PIG_ZOMBIE 
                && evt.getTarget().getType() == EntityType.PLAYER){
            
+           PigZombie pigz = (PigZombie) evt.getEntity();
+           pigz.setAngry(false);
+           
+           pigz.setTarget(null);
+           
            evt.setCancelled(true);
            
        }
    }
+   
+   @EventHandler(priority = EventPriority.HIGHEST)
+   public void EntityDamageByEntityEvent(EntityDamageByEntityEvent evt){
+       if (evt.getDamager().getType() == EntityType.PIG_ZOMBIE && evt.getEntityType() == EntityType.PLAYER){
+           evt.setCancelled(true);
+       }
+   }
+   
+   
     
 }
