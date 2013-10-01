1830eab112bd6d54ec0acb4aacade14b282b5ba3
Raymond de Vos <devosray@gmail.com>
2013-10-01 16:29:29 +0200

    Public release

---
+++ NoPigmenAgro/src/com/github/devosray/NoPigmenAgro/Plugin.java
@@ -0,0 +1,27 @@
+
+package com.github.devosray.NoPigmenAgro;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Raymond
+ */
+public class Plugin extends JavaPlugin{
+    
+    TargetListener targetl = new TargetListener(this);
+    
+    @Override
+    public void onEnable(){
+        getServer().getPluginManager().registerEvents(targetl, this);
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    
+    
+}
---
+++ NoPigmenAgro/src/com/github/devosray/NoPigmenAgro/TargetListener.java
@@ -0,0 +1,33 @@
+
+package com.github.devosray.NoPigmenAgro;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTargetEvent;
+
+/**
+ *
+ * @author Raymond
+ */
+public class TargetListener implements Listener{
+    
+    Plugin plugin;
+
+    public TargetListener(Plugin plugin) {
+        this.plugin = plugin;
+    }   
+
+   @EventHandler
+   public void onEntityTargetEvent(EntityTargetEvent evt){
+       
+       if (evt.getEntity().getType() == EntityType.PIG_ZOMBIE 
+               && evt.getTarget().getType() == EntityType.PLAYER){
+           
+           evt.setCancelled(true);
+           
+       }
+   }
+    
+}
